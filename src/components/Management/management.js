75f062736cecc0e8f9c8559b0cdd52857fd04a0d
vishnu <yvishnuvardhan325@gmail.com>
2021-11-10 13:21:27 +0530

    all pages telugu conversion is done

--- src/components/Management/management.js
+++ src/components/Management/management.js
@@ -2,8 +2,9 @@ import "./management.css";
 import trustee from "./trustee.jpg";
 import priest from "./priest.jpg";
 import CommitteeMembers from "../CommitteeMembers/committeemembers";
+import LanguageContext from "../languagecontext";
 
-const committeemembersarray=[
+const committeemembersarray_english=[
     "D.NEELAKANTAM","T.MANI","V.NAGARAJU","D.UGANDAR",
 	"B.UGANDAR",
 	"A.HEMANTH",
@@ -26,11 +27,53 @@ const committeemembersarray=[
 
 ]
 
+const committeemembersarray_telugu=[
+"డి.నీలకంఠం","టి.మణి","వి.నాగరాజు","డి.యుగంధర్",
+"బి.యుగంధర్",
+"ఎ.హేమంత్",
+"ఎ.సుబ్రహ్మణ్యం",
+"ఎ.చెంగల్రాయులు",
+"జి.వెంకటేష్",
+"ఎ.ప్రసాద్",
+"ఎ.హరి", "పి.దశరథయ్య",
+"టి.కుప్పయ్య",
+"ఎ.నితేష్",
+"డి.మిథున్",
+"ఎ.గిరి",
+"ఎ.రంగయ్య",
+"ఎన్.యెమ్మయ్య",
+"ఎన్.నాగరాజు",
+"జి.ఉదయకుమార్",
+"మునస్వామి",
+"వి.వేణుగోపాల్",
+"డి.శ్రీనివాసులు","టి.వాసు"
+]
+
+
+
+
+
 const Management=()=>{
+
+return(
+
+    <LanguageContext.Consumer>
+        {value=>{
+            const {activelanguage}=value
+
+            let requiredmembersarray=[]
+            if (activelanguage==="English"){
+                requiredmembersarray=committeemembersarray_english
+            }
+            else{
+                requiredmembersarray=committeemembersarray_telugu
+            }
+
+            
     return(
         <>
             <div className="mng-header">
-            <h1 >Management</h1>
+            <h1> {activelanguage==="English"?"Management":"నిర్వాహణ సభ్యులు"}</h1>
             <img  width="134" height="30" src="https://kanakadurgamma.org/wp-content/uploads/2021/07/motif.svg" 
                 alt="Motif_Sep"></img>
             </div>
@@ -41,8 +84,8 @@ const Management=()=>{
                     </div>
 
                     <div className="management-individual-text-container">
-                        <h2>A.MuduKrishnaiah</h2>
-                        <p>Trustee</p>
+                        <h2>{activelanguage==="English"?"A.MuduKrishnaiah":"ఎ.ముదుకృష్ణయ్య"}</h2>
+                        <p>{activelanguage==="English"?"Trustee":"ధర్మకర్త"}</p>
                         <h3>9849835844</h3>
                     </div>
                 </div>
@@ -54,8 +97,8 @@ const Management=()=>{
                     </div>
 
                     <div className="management-individual-text-container">
-                        <h2>D.krishnaiah</h2>
-                        <p>Administrator</p>
+                        <h2>{activelanguage==="English"?"D.Krishnaiah":"డి.కృష్ణయ్య"}</h2>
+                        <p>{activelanguage==="English"?"Executive":"కార్యనిర్వాహకుడు"}</p>
                         <h3>9866496375</h3>
                     </div>
                 </div>
@@ -67,20 +110,20 @@ const Management=()=>{
                     </div>
 
                     <div className="management-individual-text-container">
-                        <h2>A.Chengaiah</h2>
-                        <p>Priest</p>
+                        <h2>{activelanguage==="English"?"A.Chengaiah":"ఎ.చెంగయ్య"}</h2>
+                        <p>{activelanguage==="English"?"Priest":"పూజారి"}</p>
                         <h3>7207420472</h3>
                     </div>
                 </div>
             </div>
 
             <div className="mng-header">
-            <h1> Committee Members </h1>
+            <h1> {activelanguage==="English"?"Committee Members":"కమిటీ సభ్యులు"} </h1>
             <img  width="134" height="30" src="https://kanakadurgamma.org/wp-content/uploads/2021/07/motif.svg" 
                 alt="Motif_Sep"></img>
             </div>
             <div className="members-main-container">
-                {committeemembersarray.map((eachmember) =>{
+                {requiredmembersarray.map((eachmember) =>{
                     return <CommitteeMembers personname={eachmember} />
                 })}
             </div>
@@ -89,6 +132,14 @@ const Management=()=>{
 
         </>
     )
+
+
+        }
+
+        }
+    </LanguageContext.Consumer>
+)
+
 }
 
 
--- src/components/Temple/Topsection/topsection.js
+++ src/components/Temple/Topsection/topsection.js
@@ -2,17 +2,21 @@ import "./topsection.css";
 import top_section_image from "./top_section_image.jpg";
 
 
-const mater={
+const topsectiomater={
     English:{
         h1:"Sri Ankalammatalli Devasthanam",
         h2:"Padiredu Road,Vadamalapeta,Chittoor district, Andhrapradesh",
-        p:"   Ankalamma is counted as one of the Matrikas or Seven Mothers. She is also considered to be a form of goddess Ankali or Kali.Ankalamma is grama devata, she seems to have originated in a fierce guardian figure. Sacred areas of Ankalamma and traditional Hindu deities areworshipped symbolically to appease the inner destructive impulses in us.Fowl or lamb offerings are made in village annual ceremonies and these are later consumed as food. Ankalamma's shrines are usually located outside of the village in groves of trees. In some places shrines are located inside the villages and cities. Especially in Tamil Nadu ankalamma is worshipped by several people."
+        p1:"   Ankalamma is counted as one of the Matrikas or Seven Mothers. She is also considered to be a form of goddess Ankali or Kali.",
+        p2:"Ankalamma is grama devata, she seems to have originated in a fierce guardian figure. Sacred areas of Ankalamma and traditional Hindu deities areworshipped symbolically to appease the inner destructive impulses in us.",
+        p3:"Fowl or lamb offerings are made in village annual ceremonies and these are later consumed as food. Ankalamma's shrines are usually located outside of the village in groves of trees. In some places shrines are located inside the villages and cities. Especially in Tamil Nadu ankalamma is worshipped by several people."
     },
 
     Telugu:{
         h1:"శ్రీ అంకాలమ్మతల్లి దేవస్థానం",
         h2:"పదిరేడు రోడ్, వడమాలపేట, చిత్తూరు జిల్లా, ఆంధ్ర ప్రదేశ్",
-        p:"అంకాలమ్మ మాతృక లేదా ఏడుగురు తల్లులలో ఒకరిగా పరిగణించబడుతుంది. ఆమె అంకాలీ లేదా కాళీ దేవత రూపంగా కూడా పరిగణించబడుతుంది.అంకాలమ్మ గ్రామ దేవత, ఆమె ఒక భయంకరమైన సంరక్షక మూర్తిగా ఉద్భవించింది. మనలోని అంతర్గత విధ్వంసక ప్రేరణలను శాంతింపజేయడానికి అంకాలమ్మ మరియు సాంప్రదాయ హిందూ దేవతలకు సంబంధించిన పవిత్ర స్థలాలు ప్రతీకాత్మకంగా పూజించబడతాయి.గ్రామ వార్షిక వేడుకల్లో కోడి లేదా గొర్రె నైవేద్యాలను తయారు చేస్తారు మరియు తరువాత వీటిని ఆహారంగా తీసుకుంటారు. అంకాళమ్మ మందిరాలు సాధారణంగా ఊరి బయట చెట్ల పొదల్లో ఉంటాయి. కొన్ని ప్రదేశాలలో పుణ్యక్షేత్రాలు గ్రామాలు మరియు నగరాల లోపల ఉన్నాయి. ముఖ్యంగా తమిళనాడులో అంకాలమ్మను చాలా మంది పూజిస్తారు."
+        p1:"అంకాలమ్మ మాతృక లేదా ఏడుగురు తల్లులలో ఒకరిగా పరిగణించబడుతుంది. ఆమె అంకాలీ లేదా కాళీ దేవత రూపంగా కూడా పరిగణించబడుతుంది.",
+        p2:"అంకాలమ్మ గ్రామ దేవత, ఆమె ఒక భయంకరమైన సంరక్షక మూర్తిగా ఉద్భవించింది. మనలోని అంతర్గత విధ్వంసక ప్రేరణలను శాంతింపజేయడానికి అంకాలమ్మ మరియు సాంప్రదాయ హిందూ దేవతలకు సంబంధించిన పవిత్ర స్థలాలు ప్రతీకాత్మకంగా పూజించబడతాయి.",
+        p3:"గ్రామ వార్షిక వేడుకల్లో కోడి లేదా గొర్రె నైవేద్యాలను తయారు చేస్తారు మరియు తరువాత వీటిని ఆహారంగా తీసుకుంటారు. అంకాళమ్మ మందిరాలు సాధారణంగా ఊరి బయట చెట్ల పొదల్లో ఉంటాయి. కొన్ని ప్రదేశాలలో పుణ్యక్షేత్రాలు గ్రామాలు మరియు నగరాల లోపల ఉన్నాయి. ముఖ్యంగా తమిళనాడులో అంకాలమ్మను చాలా మంది పూజిస్తారు."
     }
 
 }
@@ -23,10 +27,10 @@ const Topsection=(props)=>{
     const gettingmater=(language)=>{
     switch (language){
         case "English":
-            return mater.English
+            return topsectiomater.English
 
         case "Telugu":
-            return mater.Telugu
+            return topsectiomater.Telugu
         default:
             return null;
     }
@@ -34,7 +38,7 @@ const Topsection=(props)=>{
 
     const {language}=props
 
-    const{h1,h2,p}=gettingmater(language)
+    const{h1,h2,p1,p2,p3}=gettingmater(language)
         return(
             <div className="top-section">
     
@@ -47,7 +51,7 @@ const Topsection=(props)=>{
         
     
                     <div className="top-section-text">
-                        <p>{p}</p>
+                        <p>{p1} <br/> <br/>{p2} <br/><br/>{p3}<br/><br/></p>
                     </div>
                 </div>
                 
